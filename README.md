9a4df71e58f756dd08adff9adb219a929dd00b2d
nhruo <nhruo123@gmail.com>
2024-01-11 11:59:27 +0200

    first try

---
+++ README.md
@@ -0,0 +1,11 @@
+Base line result
+===============
+real    2m35.141s
+user    0m0.000s
+sys     0m0.015s
+
+My first try
+============
+real    1m7.125s
+user    0m0.000s
+sys     0m0.000s
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::fs::File;
+
+use rustc_hash::FxHashMap;
+
+struct StationData {
+    min_report: f64,
+    max_report: f64,
+    report_sum: f64,
+    report_count: usize,
+}
+
+const MEASUREMENTS_FILE_PATH: &str = "../1brc/measurements.txt";
+
+fn main() {
+    let mut reports_map = FxHashMap::<&str, StationData>::default();
+    let file = File::open(MEASUREMENTS_FILE_PATH).unwrap();
+    let mmap = unsafe {
+        memmap2::MmapOptions::new()
+            .map_copy_read_only(&file)
+            .unwrap()
+    };
+
+    let mut start_of_line_index = 0;
+    let mut separator_index = 0;
+    let mut current_index = 0;
+
+    while current_index < mmap.len() {
+        match &mmap[current_index] {
+            b'\n' => {
+                let name =
+                    std::str::from_utf8(&mmap[start_of_line_index..separator_index]).unwrap();
+                let report_value = std::str::from_utf8(&mmap[separator_index + 1..current_index])
+                    .unwrap()
+                    .parse::<f64>()
+                    .unwrap();
+
+                let entry = match reports_map.entry(name) {
+                    std::collections::hash_map::Entry::Occupied(o) => o.into_mut(),
+                    std::collections::hash_map::Entry::Vacant(v) => v.insert(StationData {
+                        min_report: f64::MAX,
+                        max_report: f64::MIN,
+                        report_sum: 0.0,
+                        report_count: 0,
+                    }),
+                };
+
+                entry.max_report = entry.max_report.max(report_value);
+                entry.min_report = entry.min_report.min(report_value);
+                entry.report_sum += report_value;
+                entry.report_count += 1;
+
+                start_of_line_index = current_index + 1;
+            }
+            b';' => {
+                separator_index = current_index;
+            }
+            _ => (),
+        }
+        current_index += 1;
+    }
+    let mut reports_array = reports_map.into_iter().collect::<Vec<_>>();
+    reports_array.sort_by(|a,b| a.0.cmp(b.0));
+
+    let mut output_str = String::new();
+    output_str.push('{');
+
+    let mut add_comma = false;
+
+    for (name, report) in reports_array {
+        if add_comma {
+            output_str.push_str(", ");
+        } else {
+            add_comma = true;
+        }
+
+        let avg = (report.report_sum / report.report_count as f64 * 10.0).round() / 10.0;
+        output_str.push_str(
+            format!(
+                "{}={:.1}/{:.1}/{:.1}",
+                name, report.min_report, avg, report.max_report
+            )
+            .as_str(),
+        );
+
+    }
+
+    output_str.push('}');
+
+    println!("{}", output_str);
+}
